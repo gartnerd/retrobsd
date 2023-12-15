9b1586804969fec30b1761ab0071786fb8447686
David Gartner <gartnerd@gmail.com>
2023-12-14 17:17:25 -0700

    Updated CMakeLists.txt and mips32.toolchain.cmake in the libc diretory

--- lib/libc/mips32.toolchain.cmake
+++ lib/libc/mips32.toolchain.cmake
@@ -19,11 +19,13 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH /usr/local/mips-2013.11/bin)
 
 # Specify compiler flags - Debug, Release, RelMinSize, etc
+# There is some limit on the length of arguments. The generated make file
+# will have malformed targets. This is why this is broken into two CMAKE_C_FLAGS assignments
+# Do not quote the arguments - this will cause issues with the target creation in the make file
 set(CMAKE_C_FLAGS -mips32r2 -EL -msoft-float -nostdinc -fshort-double -Os -I${TOPSRC}/include -x assembler-with-cpp -c -B${TOPSRC}/lib/)
 set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -I${TOPSRC}/src/libc/mips/sys -I/usr/local/mips-2013.11/mips-sde-elf/include/sys -DLWHI=lwr -DLWLO=lwl -DSWHI=swr -DSWLO=swl) 
 
 # Specify linker flags - EXE,STATIC,SHARED & MODULE
-set(CMAKE_LINKER_FLAGS "${CMAKE_LINKER_FLAGS} -Wl,--oformat=elf32-tradlittlemips -N -nostartfiles -fno-dwarf2-cfi-asm -T${TOPSRC}/src/elf32-mips.ld ${TOPSRC}/src/crt0.o -L${TOPSRC}/src
-")
+set(CMAKE_LINKER_FLAGS ${CMAKE_LINKER_FLAGS} -Wl,--oformat=elf32-tradlittlemips -N -nostartfiles -fno-dwarf2-cfi-asm -T${TOPSRC}/src/elf32-mips.ld ${TOPSRC}/src/crt0.o -L${TOPSRC}/src)
 
 # Additional settings as needed
