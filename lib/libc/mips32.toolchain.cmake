13e152406f82c4dabda7bf51ad9f1a17e13ab3be
David Gartner <gartnerd@gmail.com>
2023-12-19 17:27:07 -0700

    Updated libc CMakeLists.txt (not entirely working) and the libc mips32.toolchain.cmake

--- lib/libc/mips32.toolchain.cmake
+++ lib/libc/mips32.toolchain.cmake
@@ -26,7 +26,7 @@ set(CMAKE_FIND_ROOT_PATH /usr/local/mips-2013.11/bin)
 #set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -I${TOPSRC}/src/libc/mips/sys -I/usr/local/mips-2013.11/mips-sde-elf/include/sys -DLWHI=lwr -DLWLO=lwl -DSWHI=swr -DSWLO=swl) 
 
 set(CMAKE_C_FLAGS -mips32r2 -EL -msoft-float -nostdinc -fshort-double -I${TOPSRC}/include)
-set(AS ${CMAKE_C_FLAGS} -x assembler-with-cpp -c)
+set(AS -x assembler-with-cpp -c)
 
 # Specify linker flags - EXE,STATIC,SHARED & MODULE
 set(CMAKE_LINKER_FLAGS ${CMAKE_LINKER_FLAGS} -Wl,--oformat=elf32-tradlittlemips -N -nostartfiles -fno-dwarf2-cfi-asm -T${TOPSRC}/src/elf32-mips.ld ${TOPSRC}/src/crt0.o -L${TOPSRC}/src)
