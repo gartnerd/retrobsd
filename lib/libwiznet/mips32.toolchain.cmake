806f726682a5d90ad548f8c79789e1b0bb3cd13d
David Gartner <gartnerd@gmail.com>
2023-12-31 19:29:09 -0700

    added CMakeLists.txt and mips32.toolchain.cmake for libgpanel, libreadline, libtermlib and libwiznet

---
+++ lib/libwiznet/mips32.toolchain.cmake
@@ -0,0 +1,24 @@
+# mips32.toolchain.cmake
+# https://sourcery.mentor.com/GNUToolchain/release2641
+
+set(TOPSRC /home/gartnerd/code/myretrobsd)
+
+#set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# Set the compiler
+set(CMAKE_C_COMPILER /usr/local/mips-2013.11/bin/mips-sde-elf-gcc)
+set(CMAKE_CXX_COMPILER /usr/local/mips-2013.11/bin/mips-sde-elf-g++)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR mips32)
+
+# Specify the cross-compiler prefix
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Specify the root directory of the cross-compiler
+set(CMAKE_FIND_ROOT_PATH /usr/local/mips-2013.11/bin)
+
+set(CMAKE_C_FLAGS "-mips32r2 -EL -msoft-float -nostdinc -fshort-double -I${TOPSRC}/include")
+
